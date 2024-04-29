1553957041343804eb75b3ea40a338bd569924a1
Peng Rao <raopend@gmail.com>
2024-04-29 15:21:17 +0800

    formatter

--- WeiboSpider/spiders/tweet_by_user_id.py
+++ WeiboSpider/spiders/tweet_by_user_id.py
@@ -1,16 +1,17 @@
 import datetime
 import json
-import re
 
 from scrapy import Spider
 from scrapy.http import Request
-from .common import parse_tweet_info, parse_long_tweet
+
+from .common import parse_long_tweet, parse_tweet_info
 
 
 class TweetSpiderByUserID(Spider):
     """
     用户推文数据采集
     """
+
     name = "tweet_spider_by_user_id"
 
     def start_requests(self):
@@ -18,23 +19,32 @@ class TweetSpiderByUserID(Spider):
         爬虫入口
         """
         # 这里user_ids可替换成实际待采集的数据
-        user_ids = ['1087770692']
+        user_ids = ["1223178222"]
         # 这里的时间替换成实际需要的时间段，如果要采集用户全部推文 is_crawl_specific_time_span 设置为False
         is_crawl_specific_time_span = True
-        start_time = datetime.datetime(year=2022, month=1, day=1)
-        end_time = datetime.datetime(year=2023, month=1, day=1)
+        start_time = datetime.datetime(year=2024, month=1, day=1)
+        end_time = datetime.datetime(year=2024, month=4, day=1)
         for user_id in user_ids:
             url = f"https://weibo.com/ajax/statuses/searchProfile?uid={user_id}&page=1&hasori=1&hastext=1&haspic=1&hasvideo=1&hasmusic=1&hasret=1"
             if not is_crawl_specific_time_span:
-                yield Request(url, callback=self.parse, meta={'user_id': user_id, 'page_num': 1})
+                yield Request(
+                    url, callback=self.parse, meta={"user_id": user_id, "page_num": 1}
+                )
             else:
                 # 切分成10天进行
                 tmp_start_time = start_time
                 while tmp_start_time <= end_time:
                     tmp_end_time = tmp_start_time + datetime.timedelta(days=10)
                     tmp_end_time = min(tmp_end_time, end_time)
-                    tmp_url = url + f'&starttime={int(tmp_start_time.timestamp())}&endtime={int(tmp_end_time.timestamp())}'
-                    yield Request(tmp_url, callback=self.parse, meta={'user_id': user_id, 'page_num': 1})
+                    tmp_url = (
+                        url
+                        + f"&starttime={int(tmp_start_time.timestamp())}&endtime={int(tmp_end_time.timestamp())}"
+                    )
+                    yield Request(
+                        tmp_url,
+                        callback=self.parse,
+                        meta={"user_id": user_id, "page_num": 1},
+                    )
                     tmp_start_time = tmp_end_time + datetime.timedelta(days=1)
 
     def parse(self, response, **kwargs):
@@ -42,18 +52,22 @@ class TweetSpiderByUserID(Spider):
         网页解析
         """
         data = json.loads(response.text)
-        tweets = data['data']['list']
+        tweets = data["data"]["list"]
         for tweet in tweets:
             item = parse_tweet_info(tweet)
-            item['user_id'] = response.meta['user_id']
-            item['pic_urls'] = str(item['pic_urls'])
-            del item['user']
-            if item['isLongText']:
-                url = "https://weibo.com/ajax/statuses/longtext?id=" + item['mblogid']
-                yield Request(url, callback=parse_long_tweet, meta={'item': item})
+            item["user_id"] = response.meta["user_id"]
+            item["pic_urls"] = str(item["pic_urls"])
+            del item["user"]
+            if item["isLongText"]:
+                url = "https://weibo.com/ajax/statuses/longtext?id=" + item["mblogid"]
+                yield Request(url, callback=parse_long_tweet, meta={"item": item})
             else:
                 yield item
         if tweets:
-            user_id, page_num = response.meta['user_id'], response.meta['page_num']
-            url = response.url.replace(f'page={page_num}', f'page={page_num + 1}')
-            yield Request(url, callback=self.parse, meta={'user_id': user_id, 'page_num': page_num + 1})
+            user_id, page_num = response.meta["user_id"], response.meta["page_num"]
+            url = response.url.replace(f"page={page_num}", f"page={page_num + 1}")
+            yield Request(
+                url,
+                callback=self.parse,
+                meta={"user_id": user_id, "page_num": page_num + 1},
+            )
